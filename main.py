2f198d7cd020edb689484e075024092dc874b34c
Ben Everard <bfeverard@gmail.com>
2024-10-03 15:27:49 -0400

    Built the game asteroids basic for now

--- main.py
+++ main.py
@@ -1,20 +1,54 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
+import sys
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shots import Shot
 
 def main(): 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable) 
+    AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
+    
+    asteroid_field = AsteroidField()
+
     dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0,0))
+
+        for obj in updatable:
+            obj.update(dt)
+
+        for obj in asteroids:
+            for bullet in shots:
+                if bullet.collsion(obj):
+                    bullet.kill()
+                    obj.split()
+            if obj.collsion(player):
+                print('Game Over')
+                sys.exit()
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
     
